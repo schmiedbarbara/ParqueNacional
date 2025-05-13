e341ecc069565ad8bebe1d53d99428644af830bd
Barbara Schmied <134016058+schmiedbarbara@users.noreply.github.com>
2025-05-12 22:41:44 -0300

    Update mapaPanel.java

--- src/domain/mapaPanel.java
+++ src/domain/mapaPanel.java
@@ -7,39 +7,55 @@ import java.util.Random;
 
 public class mapaPanel extends JPanel {
     private gestionParques gestor;
-    private ArrayList<Point> posiciones = new ArrayList<>();
-    private Random random = new Random();
+    private ArrayList<Point> posiciones;
+    private int[][] agm; // para guardar el AGM
 
 
     public mapaPanel(gestionParques gestor) {
         this.gestor = gestor;
-
+        this.posiciones = new ArrayList<>();
+    }
+    
+    
+    
+    public void dibujarAGM(int[][] agm) {
+        this.agm = agm;
+    }
+    
+    public void limpiarAGM() { // para limpiar el AGM
+        this.agm = null;
     }
 
-    //Inicializa gestor sin generar un parque
+    //inciializa un gestor sin crear parque
     public void inicializarGestor(gestionParques gestor) {
         this.gestor = gestor;
         repaint();
     }
 
-    //Actualiza el gestor con sus parques
+    //aca se actualiza el gestor con sus parques
     public void actualizarGestor(gestionParques gestor) {
         this.gestor = gestor;
         posicionarSenderos();
         repaint();
     }
 
+
     
     private void posicionarSenderos() {
-        if (gestor != null) {
-            int estaciones = gestor.verVecinos(0).size() + 1;
-            for (int i = 0; i < estaciones; i++) {
-                int x = 50 + random.nextInt(getWidth() - 100);
-                int y = 50 + random.nextInt(getHeight() - 100);
-                posiciones.add(new Point(x, y));
-            }
+        posiciones.clear();
+        int estaciones = gestor.tamano();
+        int radio = Math.min(getWidth(), getHeight()) / 2 - 50;
+        int centroX = getWidth() / 2;
+        int centroY = getHeight() / 2;
+
+        for (int i = 0; i < estaciones; i++) {
+            double angulo = 2 * Math.PI * i / estaciones;
+            int x = (int) (centroX + radio * Math.cos(angulo));
+            int y = (int) (centroY + radio * Math.sin(angulo));
+            posiciones.add(new Point(x, y));
         }
     }
+
     public void agregarEstacion(String nombre, int x, int y) {
         posiciones.add(new Point(x, y));
         repaint();
@@ -49,30 +65,52 @@ public class mapaPanel extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        if (gestor == null) return;
+        if (gestor == null || posiciones.size() < gestor.tamano()) return;
+        
         int tamano = posiciones.size();
 
-        // Dibuja los senderos
         for (int i = 0; i < tamano; i++) {
             for (int j : gestor.verVecinos(i)) {
-                if (i < j) { // evitar duplicar líneas
+                if (i < j) {
                     Point p1 = posiciones.get(i);
                     Point p2 = posiciones.get(j);
-                    g.setColor(Color.red.darker());
+                    g.setColor(Color.RED);
                     g.drawLine(p1.x, p1.y, p2.x, p2.y);
 
+                    int midX = (p1.x + p2.x) / 2;
+                    int midY = (p1.y + p2.y) / 2;
+                    
+                    int peso = gestor.verPesoSendero(i, j);
+                    g.setColor(Color.BLACK);
+                    g.drawString(String.valueOf(peso), midX, midY);
                 }
             }
         }
 
-        // Dibuja las estaciones
+        // aca dibuja el AGM
+        if (agm != null) {
+            for (int i = 0; i < tamano; i++) {
+                for (int j = 0; j < tamano; j++) {
+                    if (agm[i][j] > 0) { // dibuja solo si hay una conexión en el AGM
+                        Point p1 = posiciones.get(i);
+                        Point p2 = posiciones.get(j);
+                        g.setColor(Color.green.darker()); // color para el AGM
+                        g.drawLine(p1.x, p1.y, p2.x, p2.y);
+                    }
+                }
+            }
+        }
+
+        // dibuja las estaciones
         for (int i = 0; i < tamano; i++) {
             Point p = posiciones.get(i);
-            g.setColor(Color.BLUE.darker());
+            g.setColor(Color.BLUE);
             g.fillOval(p.x - 10, p.y - 10, 20, 20);
             g.setColor(Color.BLACK);
             g.drawString(String.valueOf(i), p.x - 5, p.y - 15);
         }
     }
 }
+    
+
+
