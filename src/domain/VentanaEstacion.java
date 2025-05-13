8aeb074e983cbf064288927de14fe7a04c641fe7
Barbara Schmied <134016058+schmiedbarbara@users.noreply.github.com>
2025-05-12 22:42:39 -0300

    Update VentanaEstacion.java

--- src/domain/VentanaEstacion.java
+++ src/domain/VentanaEstacion.java
@@ -8,7 +8,7 @@ public class VentanaEstacion extends JFrame {
     private gestionParques gestor = new gestionParques();
 
     public VentanaEstacion() {
-        // Inicialización VentanaInicio
+        // aca se iniciliaza la pantalla de inicio
         setTitle("PARQUE NACIONAL");
         setSize(600, 550);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -20,42 +20,40 @@ public class VentanaEstacion extends JFrame {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                // Dibuja la imagen a medida del panel
+                // dibuja la imagen del panel 
                 g.drawImage(fondo, 0, 0, getWidth(), getHeight(), this);
             }
         };
         panelFondo.setLayout(new BoxLayout(panelFondo, BoxLayout.Y_AXIS));
         panelFondo.setOpaque(false);
 
-        // Configuración del Panel Principal
         JPanel panel = new JPanel();
 
-        // Configuración del Título
+        // config  del toitulo
         JLabel titulo = new JLabel("CREA TU PARQUE NACIONAL");
-        titulo.setFont(new Font("Arial", Font.BOLD, 34)); // Ajusté el tamaño para que no se pase
+        titulo.setFont(new Font("Arial", Font.BOLD, 34)); 
         titulo.setForeground(Color.black);
         titulo.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panelFondo.add(Box.createVerticalStrut(50)); // Espaciado superior
+        panelFondo.add(Box.createVerticalStrut(50)); 
         panelFondo.add(titulo);
 
  
         
-        // Campo de texto para ingresar cantidad de estaciones
+        // campo de texto para ingresar cantidad de estaciones
         cantidadEstacionesField = new JTextField();
         cantidadEstacionesField.setPreferredSize(new Dimension(500, 30));
         cantidadEstacionesField.setAlignmentX(Component.CENTER_ALIGNMENT);
         panelFondo.add(Box.createVerticalStrut(20));
         panelFondo.add(cantidadEstacionesField);
 
-        // Inicialización de Botón
+        // inicializacion de botones
         JButton crearParqueBoton = new JButton("Crear Parque");
 
         crearParqueBoton.setPreferredSize(new Dimension(150, 40));
-        crearParqueBoton.setRolloverEnabled(true); // Cursor ilumina botón
+        crearParqueBoton.setRolloverEnabled(true); 
         JPanel botonYPreguntaPanel = new JPanel();
         botonYPreguntaPanel.setOpaque(false);
         botonYPreguntaPanel.setLayout(new FlowLayout());
-
 
         JLabel preguntaLabel = new JLabel("¿De cuántas estaciones quiere su parque?");
         preguntaLabel.setFont(new Font("Arial", Font.PLAIN, 16));
@@ -64,33 +62,27 @@ public class VentanaEstacion extends JFrame {
 
         panelFondo.add(Box.createVerticalStrut(20));
         panelFondo.add(botonYPreguntaPanel);
-
-        // Usar el panel personalizado como content pane
         setContentPane(panelFondo);
         
-
         crearParqueBoton.addActionListener(e -> {
             try {
                 int cantidad = Integer.parseInt(cantidadEstacionesField.getText());
                 JOptionPane.showMessageDialog(this, "Parque creado exitosamente. Cuentas con " + cantidad + " estaciones");
                 new interfazVisual(cantidad); // PASAMOS LA CANTIDAD
                 
-                dispose(); // Cerramos esta ventana
+                dispose(); // aca cerramos esta ventana
             } catch (NumberFormatException ex) {
                 JOptionPane.showMessageDialog(this, "Por favor ingrese un número válido.", "Error", JOptionPane.ERROR_MESSAGE);
             }
         });
         
-        // Agregar el título y el botón al panel
+        // titulo y boton al panel 
         panel.add(titulo, BorderLayout.CENTER);
         panel.add(cantidadEstacionesField, BorderLayout.CENTER);
         panel.add(crearParqueBoton, BorderLayout.SOUTH);
         panel.add(preguntaLabel,BorderLayout.NORTH);
-
-        // Agregar el panel al frame
         add(panel);
-
-        // Mostrar la ventana
         setVisible(true);
     }
 }
+
